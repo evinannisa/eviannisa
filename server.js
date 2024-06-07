255adfb069248e40013fb4bc2d196fee5cdc52b4
evinannisa <evi.annisa@students.amikom.ac.id>
2024-06-07 13:12:53 +0700

    delete build, setup fe

--- server.js
+++ server.js
@@ -11,14 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, "client", "public")));
 
-// Route untuk melayani index.html dari client/build
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// app.get("/", (req, res) => {
+//    res.send("Hello, World!");
+// });
+
+app.get("/client", (req, res) => {
+   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
-app.use("/api", contactRoute);
+app.use("/", contactRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listing to port 5000 only`));
