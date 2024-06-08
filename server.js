1f0d4b246a728f7bc0dff5b0ab9a62869bf7b78d
evinannisa <evi.annisa@students.amikom.ac.id>
2024-06-08 19:05:09 +0700

    setup route

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+   res.send("Hello, World!");
+});
+
 app.use("/", contactRoute);
 
 const port = process.env.PORT || 5000;
